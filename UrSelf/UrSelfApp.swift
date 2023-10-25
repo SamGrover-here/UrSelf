72e85970d8595d9c200a2d4ae2cd104f0976c702
Sam <samgrover221692@gmail.com>
2023-10-25 00:59:27 -0700

    Initial Commit

---
+++ UrSelf/UrSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrSelfApp.swift
+//  UrSelf
+//
+//  Created by Sam Grover on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct UrSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
